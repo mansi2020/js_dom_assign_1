9605694d3d2cad6dbf5ce816a350c8b4479bd910
mansi2020 <sorathiyamansi11@gmail.com>
2023-10-03 14:52:56 +0530

    project done

---
+++ index.js
@@ -0,0 +1,22 @@
+let val = document.getElementById('div1');
+
+//change background color
+val.style.backgroundColor = 'Yellow';
+
+//add margin 
+val.style.margin = '20px';
+
+//add padding 
+val.style.padding = '10px';
+
+//change fontsize and font weight
+val.style.fontSize = '18px';
+val.style.fontWeight = 'bold';
+
+//change height and width
+val.style.height = '200px';
+val.style.width = '300px';
+
+//change font color
+val.style.color = 'blue';
+
